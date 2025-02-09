0fef8ebcd285e5cc43d99f0c6e44dbad3cc43430
Felix Lin <felixlin77@gmail.com>
2025-02-09 20:30:19 +1100

    fix: relative asset_path for execution as Apple .app file, and Apple executable build

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,23 @@ fn main() -> GameResult {
     let mut window_mode = WindowMode::default();
     window_mode.logical_size = Some(LogicalSize::new(800.0, 600.0));
 
+    let mut exe_path = std::env::current_exe().expect("is an executable");
+    let asset_path = if exe_path
+        .parent().expect("parent is MacOS or src")
+        .ends_with("Contents/MacOS")
+    {
+        exe_path.pop();
+        exe_path.pop();
+        exe_path.push("Resources");
+        exe_path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+
     let context_builder = ContextBuilder::new("sokoban-inator", "FLIN8864")
     .window_setup(conf::WindowSetup::default().title("SOKOBAN-INATOR"))
     .window_mode(window_mode)
-    .add_resource_path(path::PathBuf::from("./resources"));
+    .add_resource_path(asset_path);
 
     let (mut context, event_loop) = context_builder.build()?;
     let game = Game::new(&mut context);
